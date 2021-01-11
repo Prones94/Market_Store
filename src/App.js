69ac1bde1e6e10a790a94de5d712cfc71aaf3a7a
Ian Rones <ronesp.85@gmail.com>
2021-01-11 12:45:07 -0800

    [add] header component and links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+// pages
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/ShopPage';
 
+// components
+import Header from './components/Header/Header';
+
+// styling
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import market from '../../assets/market.png'
+
+import './Header.styles.scss'
+
+const Header = () => (
+    <div className="header">
+    <Link className="header__logo_container" to="/">
+      <img src={market} alt="Logo" className="header__logo" />
+    </Link>
+    <div className="header__options">
+      <Link className="header__option" to="/shop">SHOP</Link>
+      <Link className="header__option" to="/shop">CONTACT</Link>
+    </div>
+    </div>
+  )
+
+export default Header
