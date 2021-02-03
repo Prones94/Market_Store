1e97b733be1862ed2ff42e15ec91eec083168cb6
Ian Rones <ronesp.85@gmail.com>
2021-02-03 00:43:59 -0800

    [add] cart.utils.js to allow clean and organized file structure

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+// Utility functions allow us to keep our files clean and organize fucntions that we may need in multiple files in one location
+
+export const addItemToCart = (cartItems, newItem) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === newItem.id
+    );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+        cartItem.id === newItem.add()
+          ? {...cartItem, quantity: cartItem.quantity + 1}
+          : cartItem
+      )
+  }
+
+  return [...cartItems, {...cartItemToAdda, quantity}]
+}
